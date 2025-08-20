af6ebaa491a2f856e5b678deee98a5a715e54124
SagarSati01 <sagarsati01@gmail.com>
2025-08-20 17:44:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css';
 import {BrowserRouter,Routes,Route}from 'react-router-dom';
 
-import Header from './components/header/header';
+import Header from './components/header/Header';
 
 import Home from './pages/home/Home';
 import Login from './pages/auth/Login';
