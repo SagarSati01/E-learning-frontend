3d9888ed4757769f9df38d053df33f6e7c239f32
SagarSati01 <sagarsati01@gmail.com>
2025-08-20 17:46:52 +0530

    Update CourseDescription.jsx

--- src/pages/coursedescription/CourseDescription.jsx
+++ src/pages/coursedescription/CourseDescription.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./Coursedescription.css";
+import "./coursedescription.css";
 import { useNavigate, useParams } from "react-router-dom";
 import { CourseData } from "../../context/CourseContext";
 import { server } from "../../main";
